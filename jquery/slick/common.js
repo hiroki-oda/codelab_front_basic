6bc515146cda1c84c08a04dd1496215852173567
hiroki-oda <hiroki.oda.b@gmail.com>
2020-03-28 11:34:16 +0900

    add_スライドショー(slick)

---
+++ jquery/slick/common.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('#slider').slick({
+    // ここにオプションを追記する
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    centerPadding: '30%',
+    focusOnSelect: true,
+    speed: 1000,
+   });
+});
