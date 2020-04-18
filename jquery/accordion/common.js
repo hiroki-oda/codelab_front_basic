5051cd542b713cc51296e9740b2cd8315b518c64
hiroki-oda <hiroki.oda.b@gmail.com>
2020-04-18 13:25:06 +0900

    fix_1_アコーディオン

--- jquery/accordion/common.js
+++ jquery/accordion/common.js
@@ -1,10 +1,9 @@
 $(function() {
   $("#accordion dd:gt(0)").hide();
-  $("#accordion dt").on("click"),function(){
-    $("#accordion dd").slideUp();
-  };
   $("#accordion dt").on("click",function(){
-    $("#accordion dd").slideUp();
-    $(this).next("dd").slideToggle();
+    if($("+dd",this).css("display")=="none") {
+      $("#accordion dd").slideUp();
+      $(this).next("dd").slideToggle();
+    }
   });
 });
